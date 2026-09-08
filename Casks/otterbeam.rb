78fee85764220cd83ad7f94ccb85a95438c8930a
otterdigitalnet <peter@otterdigital.net>
2026-09-08 12:41:26 +0800

    更新 OtterBeam cask 至 1.10.52（228）

--- Casks/otterbeam.rb
+++ Casks/otterbeam.rb
@@ -1,6 +1,6 @@
 cask "otterbeam" do
-  version "1.10.50,226"
-  sha256 "7f32db28b388da72e02f8ad6a00b0058425f30ecf25590bf23a9307d0d400da8"
+  version "1.10.52,228"
+  sha256 "5a2f74dc5a588c1f40c4336be64cdce902e99de748dd33ef313e1a7085238089"
 
   url "https://beam.otterd.com/releases/macos/OtterBeam-#{version.csv.first}+#{version.csv.second}.dmg"
   name "Otter Beam"
