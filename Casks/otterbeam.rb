c60eaba0a61d067cfd937c194c74dd83cd1c0e08
otterbeam-release <peter@otterd.com>
2026-09-15 18:34:07 +0800

    cask: otterbeam 1.11.36,305

--- Casks/otterbeam.rb
+++ Casks/otterbeam.rb
@@ -1,6 +1,6 @@
 cask "otterbeam" do
-  version "1.11.36,303"
-  sha256 "d091fdf59df20c5ca2fc6d9d1f6d7f179bb536c1731be8efa664119b058f903c"
+  version "1.11.36,305"
+  sha256 "8efb471f51f669f26f73c64fe67fd42200afe49b074725e2b6292d44dc04e097"
 
   url "https://beam.otterd.com/releases/macos/OtterBeam-#{version.csv.first}+#{version.csv.second}.dmg"
   name "Otter Beam"
